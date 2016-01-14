ce464a3473f8d1ac1e4cd501a2ff2dcc90133a11
nicothebrush <nicola.riolini@gmail.com>
2016-01-14 09:20:07 +0100

    correct bug

--- xmlrpc_operation_invoice/operation.py
+++ xmlrpc_operation_invoice/operation.py
@@ -67,7 +67,7 @@ class XmlrpcOperation(orm.Model):
                 # TODO raise
             # TODO confirm export!    
         except:    
-            _logger.error(sys_exc_info())
+            _logger.error(sys.exc_info())
             raise osv.except_osv(
                 _('Connect error:'), _('XMLRPC connecting server'))
         return res
