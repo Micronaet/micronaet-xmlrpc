37e98faf1d8902fed504dd709b8cdc6f843366b1
nicothebrush <nicola.riolini@gmail.com>
2016-08-08 18:36:08 +0200

    check another company

--- xmlrpc_operation_checkinvoice/operation.py
+++ xmlrpc_operation_checkinvoice/operation.py
@@ -105,7 +105,7 @@ class ResPartner(orm.Model):
 
         result_string_file = ''
         # TODO remove after debug:
-        for row in open('/home/thebrush/Scrivania/fattureGPB.csv'):
+        for row in open('/home/thebrush/Scrivania/fattureFIA.csv'):
             result_string_file += row            
         #res = self.pool.get('xmlrpc.operation').execute_operation(
         #    cr, uid, 'checkinvoice', parameter=parameter, context=context)            
