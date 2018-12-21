875568dbad605f463c1d8269aad7197ad1da8d2f
nicothebrush <nicola.riolini@gmail.com>
2018-12-21 15:38:51 +0100

    generic bugfix (fast message)

--- xmlrpc_operation_invoice/operation.py
+++ xmlrpc_operation_invoice/operation.py
@@ -416,9 +416,9 @@ class AccountInvoice(orm.Model):
                 pass # no Split Payment Management            
 
         # XXX Remove used for extract file:
-        open('/home/thebrush/prova.csv', 'w').write(
-            parameter['input_file_string'])
-        return False
+        #open('/home/thebrush/prova.csv', 'w').write(
+        #    parameter['input_file_string'])
+        #return False
         
         res = self.pool.get('xmlrpc.operation').execute_operation(
             cr, uid, 'invoice', parameter=parameter, context=context)
