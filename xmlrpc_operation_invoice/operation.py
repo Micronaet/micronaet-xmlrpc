6bef1c76f7e16a68836073e03c29216a9f18d5b4
nicothebrush <nicola.riolini@gmail.com>
2018-12-27 10:10:32 +0100

    generic bugfix (fast message)

--- xmlrpc_operation_invoice/operation.py
+++ xmlrpc_operation_invoice/operation.py
@@ -113,7 +113,10 @@ class AccountInvoice(orm.Model):
             # -----------------------------------------------------------------
             # Replace some not ASCII char:
             # -----------------------------------------------------------------
-            value = value.replace(u'€', u'EUR')
+            value = value.replace(u'€', u'EUR ')
+            value = value.replace(u'  ', u' ')
+            value = value.replace(u'®', u' (R)')
+            value = value.replace(u'™', u' TM')
             
             while value: # Split in 64 char:
                 # TODO change filler space
@@ -421,9 +424,9 @@ class AccountInvoice(orm.Model):
                 pass # no Split Payment Management            
 
         # XXX Remove used for extract file:
-        #open('/home/thebrush/prova.csv', 'w').write(
-        #    parameter['input_file_string'])
-        #return False
+        open('/home/thebrush/prova.csv', 'w').write(
+            parameter['input_file_string'])
+        return False
         
         res = self.pool.get('xmlrpc.operation').execute_operation(
             cr, uid, 'invoice', parameter=parameter, context=context)
