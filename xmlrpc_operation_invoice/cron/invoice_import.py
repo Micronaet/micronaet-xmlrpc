617f91684f3f6c601abe4190ec1683cf19f5fd7a
nicothebrush <nicola.riolini@gmail.com>
2020-06-09 15:00:56 +0200

    generic bugfix (fast message)

--- xmlrpc_operation_invoice/cron/invoice_import.py
+++ xmlrpc_operation_invoice/cron/invoice_import.py
@@ -79,7 +79,7 @@ for root, folders, files in os.walk(config_folder):
         # ---------------------------------------------------------------------
         # Import invoice:
         # ---------------------------------------------------------------------
-        import pdb; pdb.set_trace()
+        # import pdb; pdb.set_trace()
         while True:  # Read every time for the undo process in ODOO
             error_comment = ''
             invoice_ids = invoice_pool.search([
